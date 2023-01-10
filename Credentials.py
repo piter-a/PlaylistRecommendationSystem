806d1239827608998729ecd1d837d098b1bb14cf
Piotr Adamczyk <93036898+piter-a@users.noreply.github.com>
2023-01-10 21:42:07 +0000

    Update Credentials.py

--- Credentials.py
+++ Credentials.py
@@ -1,4 +1,4 @@
-cid = 'afc0bcf7123344d29206f0c66cee9749'
-secret = '67c8219dffeb4b6eaa80d950b51363e0' 
-redirect_uri = 'http://127.0.0.1:9090'
-user_id = 'vaynehelsing'
+cid = ''
+secret = '' 
+redirect_uri = ''
+user_id = ''
